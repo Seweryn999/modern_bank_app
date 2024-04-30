025b52312791157e7972ad4bde22b2e9146de810
Seweryn <sewcio0212@gmail.com>
2024-04-30 14:15:50 +0200

    folder changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/[modern_bank_app]/",
+  base: "/modern_bank_app/",
 });
